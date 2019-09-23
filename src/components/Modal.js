61dbb47b5d40a6b07923fac9778cb26b34e51ef8
github username <github email address>
2019-09-23 11:52:13 -0400

    added predictor form to modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -6,34 +6,70 @@ import { connect } from "react-redux";
 const ModalWrapper = styled.div`
   display: flex;
   height: 100vh;
+  /* border-style: solid;
+  border-color: white; */
 
   align-items: center;
   justify-content: center;
   align-content: center;
 
-  .modal {
+  .analytics,
+  .form {
+    border-style: solid;
+    border-color: white;
+    align-self: center;
+    margin: 2%;
+    width: 50%;
+    height: 100%;
+  }
+
+  .form {
     display: flex;
     flex-direction: column;
+
+    form {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+    }
+  }
+
+  .modal {
+    display: flex;
     align-items: center;
+    align-content: center;
     border-left: 4px solid green;
     height: 50%;
     width: 50%;
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 1);
   }
-
-  .modal div {
-    margin: 10px;
-  }
 `;
 
 export class Modal extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      date: "",
+      price: ""
+    };
+  }
+
   render() {
     return (
       <ModalWrapper>
         <div className="modal" id="modal">
-          <div className="content"></div>
-          <div className="actions">
-            <button className="toggle-button">Submit</button>
+          <div className="analytics"></div>
+          <div className="form">
+            <form>
+              <input type="text" placeholder="Name your Prediction"></input>
+              <br></br>
+              <input type="date"></input>
+              <br></br>
+              <input type="number"></input>
+              <br></br>
+              <button className="toggle-button">Submit Prediction</button>
+            </form>
           </div>
         </div>
       </ModalWrapper>
